6acadff1a8611bd078380df8090557d89b511761
Doron <62395246+DoronF3@users.noreply.github.com>
2024-08-09 14:15:59 +0300

    Delete websocket-server directory

--- websocket-server/dist/app.controller.d.ts
+++
@@ -1,8 +0,0 @@
-import { Server } from 'socket.io';
-import { EventService } from './events/event.service';
-export declare class AppController {
-    private readonly eventService;
-    constructor(eventService: EventService);
-    server: Server;
-    getHello(): Promise<void>;
-}
--- websocket-server/src/sns-subscriber/sns-subscriber.module.ts
+++
@@ -1,5 +0,0 @@
-import { ConfigurableModuleClass } from './sns-subscriber.module.definition';
-import { Module } from '@nestjs/common';
-
-@Module({})
-export class SnsSubscriberModule extends ConfigurableModuleClass {}
